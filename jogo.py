dc8bbf683c5474b007a0fc79ceabc24c57cd2acb
Victor Alexander <117864745+ViitorA@users.noreply.github.com>
2025-04-03 18:43:57 -0300

    primeiro commit do jogo base da aula de 03/04/2025
    Feito uma tela azul com uma bolinha estática no centro

---
+++ jogo.py
@@ -0,0 +1,23 @@
+from PPlay.window import *
+from PPlay.sprite import *
+
+JANELA_X = 1000
+JANELA_Y = 500
+
+def inicializar():
+    global janela;
+    janela = Window(JANELA_X,JANELA_Y)
+    janela.set_title("Victor Alexander")
+
+inicializar()
+
+# Inicializar bolinha dentro ou fora do loop?
+bolinha = Sprite("bolinha.png", frames=1);   
+
+while True:
+    janela.set_background_color([0,0,255])
+
+    bolinha.set_position(JANELA_X/2,JANELA_Y/2)
+    bolinha.draw() 
+
+    janela.update()
