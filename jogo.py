89c00bafd0e9a3cb49b75491cd80e54a3d40250a
Victor <victoralexsbr@gmail.com>
2025-04-12 01:26:09 -0300

    Aula 10/04/25, Pong parte 2; adiciona start, movimento e velocidade máxima

--- jogo.py
+++ jogo.py
@@ -1,23 +1,48 @@
 from PPlay.window import *
 from PPlay.sprite import *
 
-JANELA_X = 1000
-JANELA_Y = 500
-
 def inicializar():
     global janela;
-    janela = Window(JANELA_X,JANELA_Y)
+    janela = Window(1000,500)
     janela.set_title("Victor Alexander")
+   
+    global teclado
+    teclado = janela.get_keyboard()
+
+    init_bolinha()
+
+def init_bolinha():
+    global bolinha 
+    bolinha = Sprite("assets/bolinha.png", frames=1);   
+    bolinha.set_position( (janela.width/2)-(bolinha.width) , (janela.height/2)-(bolinha.height))
+
 
 inicializar()
 
-# Inicializar bolinha dentro ou fora do loop?
-bolinha = Sprite("assets/bolinha.png", frames=1);   
+vetor_velocidade_x = 1
+vetor_velocidade_y = 1
+VELOCIDADE_MAXIMA = 5
+
+start = False
 
 while True:
     janela.set_background_color([0,0,255])
 
-    bolinha.set_position(JANELA_X/2,JANELA_Y/2)
     bolinha.draw() 
+    
+    if(teclado.key_pressed("space")): start = True
 
-    janela.update()
+    if(start):
+        if(bolinha.x + bolinha.width > janela.width or bolinha.x < 0):
+            if(vetor_velocidade_x > VELOCIDADE_MAXIMA or vetor_velocidade_x < -VELOCIDADE_MAXIMA):
+                vetor_velocidade_x *= -1
+            else:
+                vetor_velocidade_x *= -1.2
+    
+        if(bolinha.y + bolinha.height > janela.height or bolinha.y < 0):
+            vetor_velocidade_y *= -1
+        
+        bolinha.x += vetor_velocidade_x
+        bolinha.y += vetor_velocidade_y
+
+    janela.update() # Swap buffer
